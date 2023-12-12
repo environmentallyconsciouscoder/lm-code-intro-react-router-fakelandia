af8d83129f1e4a143796ba1a880a5d64726d982d
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-12-12 21:52:35 +0000

    chore: remove comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import { BrowserRouter } from "react-router-dom";
 import Router from "./components/router/router";
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
