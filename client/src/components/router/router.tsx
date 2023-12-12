9abff2315d65bda9e46a8aed8f55b3fedb046625
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-12-12 17:46:54 +0000

    feat: added react router

---
+++ client/src/components/router/router.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import MainLayout from '../main_layout/main_layout';
+import Home from '../pages/home';
+import Confession from '../pages/confession';
+import Misdemeanour from '../pages/misdemeanour';
+import NotFound from '../not_found/not_found';
+
+function Router() {
+  return (
+    <Routes>
+        <Route path="/" element={<MainLayout />}>
+            <Route path="" element={<Home />} />
+            <Route path="/confession" element={<Confession />} />
+            <Route path="/misdemeanour" element={<Misdemeanour />} />
+            <Route path="*" element={<NotFound />} />
+        </Route>
+    </Routes>
+  )
+}
+
+export default Router
