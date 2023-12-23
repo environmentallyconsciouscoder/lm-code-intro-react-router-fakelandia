f174c728ebb86b11338e79553e7e82d8b26dc002
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-12-23 01:00:01 +0000

    feat: added a useEffect to set state when misdeameanours are passed down from provider

--- client/src/components/pages/home.tsx
+++ client/src/components/pages/home.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { MisdeameanourContext } from '../context/data_provider';
 import { Misdemeanour } from '../types/misdeameanour.types';
 import Card from '../features/card/card';
@@ -6,14 +6,21 @@ import Card from '../features/card/card';
 function Home() {
   const userContext = useContext(MisdeameanourContext);
   const [selectedFilter, setSelectedFilter] = useState('All'); // Step 1
+  const [misdeameanours, setMisdeameanours] = useState< never[] | [Misdemeanour]>([]);
 
-  const misdeameanours = userContext.data === null ? [] : userContext.data.misdemeanours;
+  // const misdeameanours = userContext.data === null ? [] : userContext.data.misdemeanours;
+
+  useEffect(() => {
+    const results = userContext.data === null ? [] : userContext.data.misdemeanours;
+    console.log("results", results)
+    setMisdeameanours(results)
+  },[userContext.data])
 
   const handleFilterChange = (event: { target: { value: string; }; }) => {
     setSelectedFilter(event.target.value);
   };
 
-  const filteredMisdeameanours = selectedFilter === "All" ? misdeameanours: misdeameanours.filter((data: { misdemeanour: string; }) => data.misdemeanour === selectedFilter);
+  const filteredMisdeameanours = selectedFilter === "All" ? misdeameanours: misdeameanours?.filter((data: { misdemeanour: string; }) => data.misdemeanour === selectedFilter);
 
   return (
     <div style={{height: '100%', overflow: 'auto'}}>
